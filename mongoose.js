0b3ec2df71677cf07857df7d793fe6ac5f4df5e5
Mangala Nayak <mangala.nayak@robosoftin.com>
2019-03-21 20:19:28 +0530

    Added nayak

--- mongoose.js
+++ mongoose.js
@@ -3,5 +3,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/FourSquareAppDatabase');
-
+Hi I'm Mangala Nayak. 
+Thank you.
 module.exports = {mongoose};

8703bad1db8297e2f583b2fa0a5328a795e24a7e
Glanis <glanis.monteiro@robosoftin.com>
2019-03-21 20:06:34 +0530

    my first commit

--- mongoose.js
+++ mongoose.js
@@ -4,4 +4,5 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/FourSquareAppDatabase');
 
+console.log("this is a new line")
 module.exports = {mongoose};
